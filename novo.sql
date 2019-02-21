8b25e27f4a47c63d6fa90fd7835523714e92387b
= <=>
2019-02-21 19:10:35 -0300

    Começando a insert

--- novo.sql
+++ novo.sql
@@ -2,21 +2,12 @@ create database novo character set = utf8;
 use novo;
 /*Não deixar nada por padrão sempre colocar 
 */
-
-create table tblProprietario(
+create table tblproprietario(
 	strNome varchar(50) not null,
-	strCpf char(11)  not null,
+	strCpf char(11)  not null unique,
 	dateNasc date not null, 
 	pkProprietario integer not null primary key auto_increment,
 	strObservacao varchar(200) null
-	) IENGINE ;
-	
-	
-create table tblPet(
-	strNome varchar(50),
-	strEspecie varchar(50),
-	strRga varchar(20),
-	pkPet integer,
-	fkProprietario integer,
-	foreign key (fkProprietario) references tblProprietario(pkProprietario)
-);
+	) ENGINE innoDB character set = utf8;
+
+--INSERT
