a7ecf0864c4146932f134390952628a1ef234cb2
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-03-21 21:23:12 -0300

    Feito hoje

---
+++ DBescola.sql
@@ -0,0 +1,108 @@
+CREATE DATABASE dbModelo;
+USE dbModelo;
+
+CREATE TABLE tblestudante(
+    pkChave integer not null primary key auto_increment,
+    dcCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    dcModified DATETIME NULL ON UPDATE CURRENT_TIMESTAMP,
+    strProntuario char(9) unique,
+    strNome varchar(50),
+    dtNascimento date
+)ENGINE = InnoDB CHARACTER SET = utf8;
+
+CREATE TABLE tblturma(
+    dcCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    dcModified DATETIME NULL ON UPDATE CURRENT_TIMESTAMP,
+    pkChave integer not null primary key auto_increment,
+    strDia varchar(6) not null,
+    strPeriodo varchar(6) not null
+)ENGINE = InnoDB CHARACTER SET = utf8;
+
+
+CREATE TABLE relestudanteturma(
+    dcCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    dcModified DATETIME NULL ON UPDATE CURRENT_TIMESTAMP,
+    pkChave integer not null primary key auto_increment,
+    fkEstudante integer not null,
+    fkTurma integer not null,
+    foreign key (fkEstudante) references tblestudante(pkChave),
+    foreign key (fkTurma) references tblturma(pkChave),
+    unique(fkEstudante, fkTurma)
+)ENGINE = InnoDB CHARACTER SET = utf8;
+
+
+CREATE TABLE tbldisciplina(
+    dcCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    dcModified DATETIME NULL ON UPDATE CURRENT_TIMESTAMP,
+    pkChave integer not null primary key auto_increment,
+    strSigla char(5) not null unique,
+    strEscolaridade varchar(20) null
+)ENGINE = InnoDB CHARACTER SET = utf8;
+
+CREATE TABLE reldisciplinaturma(
+    dcCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    dcModified DATETIME NULL ON UPDATE CURRENT_TIMESTAMP,
+    pkChave integer not null primary key auto_increment,
+    fkTurma integer not null,
+    fkDisciplina integer not null,
+    foreign key (fkTurma) references tblturma(pkChave),
+    foreign key (fkDisciplina) references tbldisciplina(pkChave),
+    unique (fkTurma)
+)ENGINE = InnoDB CHARACTER SET = utf8;
+
+CREATE TABLE tbldocente(
+    dcCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    dcModified DATETIME NULL ON UPDATE CURRENT_TIMESTAMP,
+    pkChave INTEGER NOT NULL PRIMARY KEY auto_increment,
+    strProntuario char(9) not null unique,
+    strNome varchar(50) not null,
+    strEscolaridade varchar(20) not null, 
+    dtNascimento date
+)ENGINE = InnoDB CHARACTER SET = utf8;
+
+CREATE TABLE reldocentedisciplina(
+    dcCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    dcModified DATETIME NULL ON UPDATE CURRENT_TIMESTAMP,
+    pkChave integer primary key not null auto_increment,
+    fkDocente integer not null,
+    fkDisciplina integer not null,
+    foreign key (fkDocente) references tbldocente(pkChave),
+    foreign key (fkDisciplina) references tbldisciplina(pkChave),
+    unique(fkDocente, fkDisciplina)
+)ENGINE = InnoDB CHARACTER SET = utf8;
+
+
+
+
+
+INSERT INTO tblestudante(strProntuario, strNome, dtNascimento) values ("SP3003030", "Joao", "1999-10-23");
+INSERT INTO tblestudante(strProntuario, strNome, dtNascimento) values ("SP3003031", "Claudio", "1999-10-23");
+INSERT INTO tblestudante(strProntuario, strNome, dtNascimento) values ("SP3003033", "Jobiscley", "1999-10-23");
+INSERT INTO tblestudante(strProntuario, strNome, dtNascimento) values ("SP3003034", "Joelington", "1999-10-23");
+
+
+
+INSERT INTO tbldocente(strProntuario, strNome, strEscolaridade ,dtNascimento) values ("SP3003032", "Irineu", "doutor","1999-10-23");
+
+INSERT INTO tbldisciplina(strSigla) values ("BD2A3");
+INSERT INTO tblturma(strDia, strPeriodo) values ("SEG", "3");
+
+INSERT INTO reldisciplinaturma(fkDisciplina, fkTurma) values (01, 01);
+INSERT INTO reldocentedisciplina(fkDocente, fkDisciplina) values(01, 01);
+INSERT INTO relestudanteturma(fkEstudante, fkTurma) values(01, 01);
+INSERT INTO relestudanteturma(fkEstudante, fkTurma) values(02, 01);
+INSERT INTO relestudanteturma(fkEstudante, fkTurma) values(03, 01);
+
+CREATE FUNCTION fnNovaChave() RETURNS BINARY(16) DETERMINISTIC
+	RETURN UNHEX(REPLACE(UUID(), '-', ''));
+	
+DELIMITER //	
+CREATE PROCEDURE prCadastrarDocente (OUT varProntuario varchar(9), varNome varchar(50), varEscolaridade varchar(10), varNascimento date)
+	BEGIN
+		START TRANSACTION
+			INSERT INTO tbldocente(upkDocente, strProntuario, strNome, strEscolaridade, dtNascimento) values (fnNovaChave(), varProntuario, varNome, varEscolaridade, varNascimento);
+
+			COMMIT;
+		END //
+		
+DELIMITER;
